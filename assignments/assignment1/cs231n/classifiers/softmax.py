fcabed784f027a81f0849e94cc2bb3602b6dc5e1
Brian Yoon <byoon2002@gmail.com>
2021-09-27 14:25:53 +0000

    completed unvectorized/vectorized softmax loss and grad

--- assignments/assignment1/cs231n/classifiers/softmax.py
+++ assignments/assignment1/cs231n/classifiers/softmax.py
@@ -34,8 +34,32 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
+    
 
+    for i in range(num_train):
+      scores = X[i].dot(W)
+      correct_score = scores[y[i]]
+      loss += (-1*correct_score + np.log(np.exp(scores).sum()))
+      for c in range(num_classes):
+        adjusted_scores = scores - np.max(scores)
+        softmax = np.exp(adjusted_scores) / np.sum(np.exp(adjusted_scores))
+        dW[:,c] += X[i] * softmax[c]
+      dW[:,y[i]] -= X[i]
+    loss /= num_train
+    loss += reg * (W**2).sum()
+    dW /= num_train
+    dW += reg * 2 * W
+    
+
+    # d(X[i].dot(W))/d(W) = X[i] therefore dL/dW = dL/df * X[i]
+    # So just find dLi / dfyi for yi = j and yi != j
+    # when you do chain rule on summation part, the derivative of sum(e^fj) is e^fj
+    # https://stackoverflow.com/questions/41663874/cs231n-how-to-calculate-gradient-for-softmax-loss-function
+
+    # Bug encountered with gradient check was that gradient check uses the loss value. the loss value was false because
+    # the correct_score class was not included in summation
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
@@ -50,6 +74,8 @@ def softmax_loss_vectorized(W, X, y, reg):
     # Initialize the loss and gradient to zero.
     loss = 0.0
     dW = np.zeros_like(W)
+    num_train = X.shape[0]
+    num_classes = W.shape[1]
 
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
@@ -58,6 +84,21 @@ def softmax_loss_vectorized(W, X, y, reg):
     # regularization!                                                           #
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+
+    scores = X.dot(W)
+    correct_score = scores[np.arange(num_train), y]
+    loss = (-1 * correct_score.sum() + np.log(np.exp(scores).sum(axis=1)).sum()) / num_train + np.sum(W**2)
+    stablized_scores = scores - np.max(scores, axis=1).reshape((num_train, 1))
+    softmax = np.exp(stablized_scores) / np.sum(np.exp(stablized_scores), axis=1).reshape((num_train,1))
+    softmax[np.arange(num_train), y] -= 1
+    dW = X.T.dot(softmax)
+    dW /= num_train
+    dW += reg * 2 * W
+
+    # 1.Have to sum inside log experession then sum the logs. Cant just sum the two d array
+    # 2. Need to sum stablized_scores in axis=1 and then reshape to get desired softmaxes
+    # 3. had same problem with stablized scores. always check shapes of values being calculated to avoid miscalculations in wrong axis
+  
 
     pass
 
