found bug in softmax loss layer that prevented it from running in both dropout AND assignment1 loss check bug free

assignments/assignment2/cs231n/layers.py
@@ -198,10 +198,10 @@ def softmax_loss(x, y):
     correct_scores = x[np.arange(y.shape[0]), y]
     loss = -1 * correct_scores + np.log(np.exp(x).sum(axis=1))
     loss = loss.sum() / y.shape[0]
-    x -= np.max(x, axis=1).reshape((y.shape[0], 1))
-    dx = np.exp(x) / np.sum(np.exp(x), axis=1).reshape((y.shape[0],1))
+    adjusted = x - np.max(x, axis=1).reshape((y.shape[0], 1))
+    dx = np.exp(adjusted) / np.sum(np.exp(adjusted), axis=1).reshape((y.shape[0],1))
     dx[np.arange(y.shape[0]), y] -= 1
-    dx /= y.shape[0]
+    dx /= x.shape[0]
 
 
 